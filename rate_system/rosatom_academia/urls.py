e9c7507bda390dd959e49283f11da087540dca8f
Arsenii Kropotov <a.simakin.2002@gmail.com>
2023-04-30 01:25:02 +0300

    senya_fix2

--- rate_system/rosatom_academia/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import home_page
-from django.urls import path, include
-
-
-urlpatterns = [
-    path('', home_page, name = "home_page"),
-    # path('review/', , name='review'),
-    # path('home/', index_page, name='index')
-]
